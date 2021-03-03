fa74c5f4fef28f250079843ec6d06c922791aa42
Urazof <2778665@gmail.com>
2021-03-03 15:32:12 +0200

    Added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {INTERVAL, N_COLUMNS, N_ROWS} from "./config/config";
 import Game from "./components/Game";
 
-export default function App(){
-  return (<div>
-    {/*<button> Start game</button>*/}
-    <Game name="Game of Live" columns={N_COLUMNS} rows={N_ROWS} interval={INTERVAL}/>
-    {/*<Game name="Game of Live2" columns={N_COLUMNS} rows={N_ROWS} interval={INTERVAL}/>*/}
-    {/*<Game name="Game of Live3" columns={N_COLUMNS} rows={N_ROWS} interval={INTERVAL}/>*/}
-  </div>);
+
+function handleClick() {
+    return (
+        <div>
+            <Game name="Game of Live" columns={N_COLUMNS} rows={N_ROWS} interval={INTERVAL}/>
+            {/*<Game name="Game of Live" columns={N_COLUMNS} rows={N_ROWS} interval={INTERVAL}/>*/}
+            {/*<Game name="Game of Live2" columns={N_COLUMNS} rows={N_ROWS} interval={INTERVAL}/>*/}
+            {/*<Game name="Game of Live3" columns={N_COLUMNS} rows={N_ROWS} interval={INTERVAL}/>*/}
+        </div>
+    );
+    console.log('The link was clicked.');
+}
+
+const res = handleClick()
+
+export default function App() {
+    const [start, setStart] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setStart(true)}>StartGame</button>
+            <button onClick={() => setStart(false)}>StopGame</button>
+            {start ? res : ''}
+
+        </>
+    )
 }
